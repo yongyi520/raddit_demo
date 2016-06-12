e1b9957db0cb29435799b793301c5042b4716690
yongyi <reach.yongyixu@gmail.com>
2016-06-12 05:49:58 -0400

    association between link and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612093516) do
+ActiveRecord::Schema.define(version: 20160612094741) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.string   "url",        limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
