a8fab6f200dcef0e4e0ebe0da59ce5ce5e063618
yongyi <reach.yongyixu@gmail.com>
2016-06-13 03:47:36 -0400

    styled the whole page

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'devise', '~> 4.1', '>= 4.1.1'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612094741) do
+ActiveRecord::Schema.define(version: 20160613072629) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160612094741) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
